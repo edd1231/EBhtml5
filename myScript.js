26c90dd62855cdae42c66d7473dffd73f85f0239
edd1231 <bornliveandedd@hotmail.com>
2017-06-01 10:35:27 +0100

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,14 @@
+//This is my Javascript file for the AJAX exercise
+/*var myManufacturer = document.getElementById("manufacturer");
+myManufacturer.addEventListener("change", loadMyData, false);
+
+function loadMyData() {
+    "use strict";
+    alert("its working!!!");
+}*/
+
+
+
+
+
+
